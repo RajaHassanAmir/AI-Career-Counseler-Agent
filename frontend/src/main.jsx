a35d7416490342b4700d747582d4629e06e3b34d
RajaHassanAmir <rajahassanamir3@gmail.com>
2026-05-29 11:30:32 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./Context/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
