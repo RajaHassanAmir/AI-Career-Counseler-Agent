1ac662cfcc21990b32fa88e8b44c74ff3d321402
RajaHassanAmir <rajahassanamir3@gmail.com>
2026-05-29 11:39:40 +0500

    Update navbar.jsx

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../Context/AuthContext";
 
 import {
   useNavigate,
